49c2118abf3aabd5336172ca4fd73cb5b16806f9
Sourav2707 <smartsourav33@gmail.com>
2022-07-12 21:02:37 +0530

    [Sourav] - adding options

--- SnakeLadder/SnakeLadder/Snake.cs
+++ SnakeLadder/SnakeLadder/Snake.cs
@@ -11,33 +11,38 @@ namespace SnakeLadder
         public const int ladder = 1, snake = 2;
         public void Ladder()
         {
-            int position = 0;
-            Console.WriteLine($"The starting position of player is {position}");
-            Random random = new Random();
-            int dice = random.Next(1, 7);
-            Console.WriteLine($"The number got by dice roll is {dice}");
+            int position1 = 0;
+            Console.WriteLine($"The starting position of player is {position1}");
+            Console.WriteLine("\n");
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine($"The number got by dice roll is {dice}");
                 Random random2 = new Random();
                 int option = random2.Next(0, 3);
                 switch (option)
                 {
                     case ladder:
-                        position += dice;
+                        position1 += dice;
                         Console.WriteLine("The player climbed the ladder");
                         break;
                     case snake:
-                        position -= dice;
-                        if(position < 0)
+                        position1 -= dice;
+                        if (position1 < 0)
                         {
-                        Console.WriteLine("The player bit by snake");
-                        position = 0;
+                            Console.WriteLine("The player bit by snake");
+                            position1 = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("The player bit by snake");
                         }
                         break;
                     default:
-                        position = 0;
                         Console.WriteLine($"No play");
                         break;
                 }
-            Console.WriteLine($"The player position is {position}");
+                Console.WriteLine($"The player position is {position1}");
+                Console.WriteLine("\n");
         }
     }
 }
