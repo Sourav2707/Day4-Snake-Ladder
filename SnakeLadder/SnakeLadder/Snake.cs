2c6f3e2b5624426a4969448edcb1b6fa54bf6298
Sourav2707 <smartsourav33@gmail.com>
2022-07-12 18:27:57 +0530

    [Sourav] - adding options

--- SnakeLadder/SnakeLadder/Snake.cs
+++ SnakeLadder/SnakeLadder/Snake.cs
@@ -8,6 +8,7 @@ namespace SnakeLadder
 {
     public class Snake
     {
+        public const int ladder = 1, snake = 2;
         public void Ladder()
         {
             int position = 0;
@@ -15,6 +16,30 @@ namespace SnakeLadder
             Random random = new Random();
             int dice = random.Next(1, 7);
             Console.WriteLine($"The number got by dice roll is {dice}");
+                Random random2 = new Random();
+                int option = random2.Next(0, 3);
+                switch (option)
+                {
+                    case ladder:
+                        position += dice;
+                        Console.WriteLine($"The player climed the ladder and moved ahead to the position {position}");
+                        break;
+                    case snake:
+                        position -= dice;
+                        if(position < 0)
+                        {
+                            Console.WriteLine($"The player bit by snake and the position is 0");
+                        }
+                        else
+                        { 
+                            Console.WriteLine($"The player bit by snake and moved behind to the position {position}"); 
+                        }
+                        break;
+                    default:
+                        position = 0;  
+                        Console.WriteLine($"No play and the player stayed in the same position {position}");
+                        break;
+                }
         }
     }
 }
