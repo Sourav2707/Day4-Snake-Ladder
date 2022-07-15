61a6daa369029e93c8d5cb092539097bacc8200c
Sourav2707 <smartsourav33@gmail.com>
2022-07-15 12:26:35 +0530

    adding 2nd player

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -1,2 +1,4 @@
-﻿Console.WriteLine("Welcom to Snake and Ladder program");
-//this program may take 5-10 minutes to show the result
+﻿using SnakeLadder;
+Console.WriteLine("Welcom to Snake and Ladder program");
+Snake snake = new Snake();
+snake.Ladder();
